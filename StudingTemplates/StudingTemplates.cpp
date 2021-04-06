d77fd1fcf80d245d3a58b52f1dcdad8094853edf
inozemcev (windows) <= officefish>
2021-04-07 01:38:33 +0300

    Optional fixes

--- StudingTemplates/StudingTemplates.cpp
+++ StudingTemplates/StudingTemplates.cpp
@@ -4,58 +4,26 @@
 
 #include <functional>
 #include<typeindex>
+#include<optional>
+#include<type_traits>
 
 #include "list/singly/hasky.h"
+
+
 
 int main() {
 
 	auto sm = term::legion::sum<int>();
 	auto ml = term::legion::mul<int>();
 
-	//Hasky list{ ml, ml, sm, sm, sm };
-	//std::any value = list.reduce(1);
+	AnyProccessor list{ ml, ml, sm, sm, sm };
+	std::any value = list.reduce(1);
 
-	Hasky list2{ ml };
-	std::any value2 = list2.reduce({ 2, 2 });
+	std::cout << std::any_cast<int>(value) << std::endl; // output: 4096 // (64*(8*(4+(2+(1+1)))))
 
-	//Hasky list3{ sm };
-	//std::any value3 = list3.reduce({ 3, 3, 4 });
+	AnyProccessor list2{ ml };
+	std::any value2 = list2.reduce({ 1, 2, 3, 4, 5 });
 
-	//Hasky list4{ sm };
-	//std::vector<std::any> constants{ 1, 2, 3, 4, 5 };
-	//std::any value4 = list4.reduce(constants);
-
-	//std::cout << std::any_cast<int>(value) << std::endl; // output: 4096 // (64*(8*(4+(2+(1+1)))))
-	std::cout << std::any_cast<int>(value2) << std::endl; // output: 4 (2+2)
-	//std::cout << std::any_cast<int>(value3) << std::endl; // output: 10 (3+3+4)
-	//std::cout << std::any_cast<int>(value4) << std::endl; // output: 10 (1+2+3+4+5)
-
-	//for (auto&& v : list3) {
-		//std::cout << v << " ";
-	//}
-
-	//static_assert(list3.accumulate([](int32_t i) {return i; }, 0) == 15, "Wrong");
-	//static_assert(sll::Accumulate(flist, [](int i) {return i; }, 0) == 15);
-
-	//static_assert(sll::(list2) == "", "");
-	//std::cout << sll::toString(list2) << '\n';
-
-	//list2.toString();
-	//list3.toString();
-	//list4.toString();
-	//list5.toString();
-	//hasky.print();
-	//hasky.print();
-	//const hasky list_const{ 1,2,3,4,5 };
-
-	// member functions
-	//hasky list1;
-	//list1.assign(10, 1).print();
-	//hasky list2;
-	//list2.assign(list1.begin(), list1.end()).print();
-	//hasky list3;
-	//list2.assign({1,2,3,4,5,6,7,8,9}).print();
-
-
+	std::cout << std::any_cast<int>(value2) << std::endl; // output: 120 // (1*(2*(3*(4*(5)))))
 
 }
--- StudingTemplates/list/singly/hasky.h
+++ StudingTemplates/list/singly/hasky.h
@@ -13,78 +13,94 @@
 #include<queue>
 #include<stack>
 
-namespace sigma {
+namespace utilities {
+
+	template <typename T>
+	auto get_v_opt(const std::any& any) noexcept -> std::optional<T>  {
+		return std::any_cast<T>(&any) ? *std::any_cast<T>(&any) : std::optional<T>();
+	}
 
 	template <class... Args>
-	auto any_to_variant_cast(std::any a)  -> std::variant<Args...> {
-		if (!a.has_value())
-			throw std::bad_any_cast();
-		std::optional<std::variant<Args...>> v = std::nullopt;
-		bool found = ((a.type() == typeid(Args) && (v = std::any_cast<Args>(std::move(a)), true)) || ...);
-		if (!found)
-			throw std::bad_any_cast{};
-		return std::move(*v);
+	auto any_to_variant_cast(std::any any) noexcept -> std::optional<std::variant<Args...>> {
+		if (not any.has_value()) return std::nullopt;
+		std::optional<std::variant<Args...>> variant = std::nullopt;
+		((variant = get_v_opt<Args>(any), true) || ...);
+		return variant;
 	}
+}
+
+namespace sigma {
+
+	using utilities::any_to_variant_cast;
+	using utilities::get_v_opt;
 
 	template <class T>
 	class variadic_container {
+
 #define ANY_VARIANTS std::initializer_list<std::any>, std::vector<std::any>
 #define VARIANTS std::initializer_list<T>, std::vector<T>
 		using container_variant = std::variant<VARIANTS>;
-
+		using variant_optional = std::optional<std::variant<ANY_VARIANTS>>;
 	private:
 		std::any _container;
 
 	public:
-		template<class C> variadic_container(const C& c) : _container(c) {}
-		template<class C> variadic_container(C&& c) : _container(c) {}
+		template<class C> constexpr variadic_container(const C& c) noexcept : _container(c) {};
+		template<class C> constexpr variadic_container(C&& c) noexcept : _container(c) {};
 
 		template<class C>
-		variadic_container& operator=(const C& c) { _container = c; return *this; }
+		variadic_container& operator=(const C& c) noexcept { _container = c; return *this; }
 		template<class C>
-		variadic_container& operator=(C&& c) { _container = std::move(c); return *this; }
+		variadic_container& operator=(C&& c) noexcept { _container = std::move(c); return *this; }
 
 		template <class T>
-		T everyValue(std::function<T(T, T)> algebra) {
-			auto variant = any_to_variant_cast<ANY_VARIANTS>(_container);
+		T constexpr everyValue(std::function<T(T, T)> algebra) noexcept {
+			variant_optional variantOpt = any_to_variant_cast<ANY_VARIANTS>(_container);
+			if (not variantOpt) return 0;
 			return std::visit([this, algebra](auto& variant) {
-				T value = std::any_cast<T>(*variant.begin());
-				for (auto it = std::next(variant.begin()); it != variant.end(); ++it)
-					value = algebra(value, std::any_cast<T>(*it));
+				std::optional<T> valueOpt = get_v_opt<T>(*variant.begin());
+				if (not valueOpt) return 0;
+				T value = *valueOpt;
+				for (auto it = std::next(variant.begin()); it != variant.end(); ++it) {
+					valueOpt = *std::any_cast<T>(&(*it));
+					if (not valueOpt) return 0;
+					value = algebra(value, *valueOpt);
+				}
 				return value;
-			}, variant);
-
+			}, *variantOpt);
+			return 0;
 		}
 	};
 }
 
-class Node {
+
+class AnyNode {
 public:
 
-	using value = std::function<std::any(std::any)> ;
+	using value = std::function<std::any(std::any)>;
 
-	std::shared_ptr<Node> next = nullptr;
-	std::function < std::any(std::any) > data;
+	std::shared_ptr<AnyNode> next = nullptr;
+	std::function <std::any(std::any)> data;
 
-	Node() = default;
-	~Node() = default;
-	Node(value _data) : data(_data) {};
+	AnyNode() = default;
+	~AnyNode() = default;
+	AnyNode(value _data) : data(_data) {};
 };
 
-class Hasky {
-	using node = std::shared_ptr<Node>;
+class AnyProccessor {
+	using node = std::shared_ptr<AnyNode>;
 	using value = std::function <std::any(std::any)> ;
 public:
 	node head = nullptr;
-	Hasky() = delete;
-	~Hasky() = default;
-	Hasky(value data) {
+	AnyProccessor() = delete;
+	~AnyProccessor() = default;
+	AnyProccessor(value data) {
 		this->head = Insert(this->head, data);
 	}
-	Hasky(const std::initializer_list<value>& list) {
+	AnyProccessor(const std::initializer_list<value>& list) {
 		this->head = Merge(this->head, list);
 	}
-	Hasky& insert(value data) {
+	AnyProccessor& insert(value data) {
 		this->head = Insert(this->head, data);
 		return *this;
 	}
@@ -93,20 +109,28 @@ public:
 		return Terminate(this->head);
 	}
 
-	std::any reduce (std::any constant) {
+	std::any reduce(std::any constant) noexcept {
 		return Reduce(this->head, constant);
 	}
-	std::any reduce(const std::initializer_list<std::any>& list) {
+	std::any reduce(const std::initializer_list<std::any>& list) noexcept {
 		return Reduce(this->head, list);
 	}
 
+	template<typename T>
+	T reduce_and_cast(const std::initializer_list<std::any>& list) noexcept {
+		std::any value = Reduce(this->head, list);
+		std::optional<T> valueOpt = *std::any_cast<T>(&value);
+		if (valueOpt) return *valueOpt;
+		return 0;
+	}
+
 	static node Insert(node head, value data) {
-		node newNode = std::make_shared<Node>(data);
+		node newNode = std::make_shared<AnyNode>(data);
 		if (head) newNode->next = head;
 		return newNode;
 	}
 	static node BackInsert(node head, value data) {
-		node newNode = std::make_shared<Node>(data);
+		node newNode = std::make_shared<AnyNode>(data);
 		if (not head) return newNode;
 		node current = head;
 		while (current->next)
@@ -116,10 +140,10 @@ public:
 	}
 
 	static node Merge(node head, const std::initializer_list<value>& list) {
-		node dummy = std::make_shared<Node>();
+		node dummy = std::make_shared<AnyNode>();
 		node current = dummy;
 		for (auto& data : list) {
-			node tmp = std::make_shared<Node>(data);
+			node tmp = std::make_shared<AnyNode>(data);
 			current->next = tmp;
 			current = current->next;
 		}
@@ -128,7 +152,7 @@ public:
 		return dummy;
 	}
 
-	static std::any Terminate(node head) {
+	static std::any Terminate(node head) noexcept {
 		node current = head;
 		if (not current->next) {
 			std::any gg;
@@ -156,6 +180,7 @@ public:
 		head = BackInsert(head, cnst);
 		return Terminate(head);
 	}
+
 	static std::any Reduce(node head, std::any constant) {
 		std::function <std::any(std::any)> cnst = Constant(constant);
 		head = BackInsert(head, cnst);
@@ -171,17 +196,27 @@ public:
 	static constexpr auto mul = [&](T a, T b) { return a * b; };
 };
 
+template<typename, typename = void>
+constexpr bool is_type_complete_v = false;
+
+template<typename T>
+constexpr bool is_type_complete_v
+<T, std::void_t<decltype(sizeof(T))>> = true;
+
 namespace term::legion {
+
+	using utilities::get_v_opt;
 
 	using std::function, std::any, std::any_cast, std::type_index;
 	using variadic = sigma::variadic_container<any>;
 
 	template<typename T>
-	function<any(any)> legion(const function<T(T, T)>& algebra) { // std::function<T(T,T...)
+	constexpr function<any(any)> legion(const function<T(T, T)>& algebra) noexcept { // std::function<T(T,T...)
 		function<any(any)> redex = [algebra](any input) {
-			type_index index = type_index(input.type());
-			if (index == type_index(typeid(T)))
-				return algebra(any_cast<T>(input), any_cast<T>(input));
+			std::optional<T> valueOpt = get_v_opt<T>(input);
+			if (valueOpt)
+				return algebra(*valueOpt, *valueOpt);
+
 			variadic variadic_container(input);
 			return variadic_container.everyValue<T>(algebra);
 		};
@@ -189,11 +224,101 @@ namespace term::legion {
 	}
 
 	template<typename T>
-	std::function<std::any(std::any)> sum() {
+	constexpr std::function<std::any(std::any)> sum() noexcept {
 		return legion<T>(Calculator<T>::sum);
 	};
 	template<typename T>
-	std::function<std::any(std::any)> mul() {
+	constexpr std::function<std::any(std::any)> mul() noexcept {
 		return legion<T>(Calculator<T>::mul);
 	};
 }
+
+
+
+/*
+template <typename ...Variants>
+class VariantNode {
+public:
+	using variants = std::variant<Variants...>;
+	using Value = std::function<variants(variants)>;
+	using Node = std::shared_ptr<VariantNode<Variants...>>;
+
+	std::optional<Node> next = std::nullopt;
+	Value data;
+
+	VariantNode() = default;
+	~VariantNode() = default;
+	constexpr explicit VariantNode(Value _data) : data(_data)  {};
+};
+
+template <typename ...Variants>
+class VariantProccessor {
+	using variants = std::variant<Variants...>;
+	using Value = std::function<variants(variants)>;
+	using NodeType = VariantNode<Variants...>;
+	using Node = std::shared_ptr<NodeType>;
+
+public:
+	std::optional<Node> head = std::nullopt;
+	VariantProccessor() = default;
+	~VariantProccessor() = default;
+	constexpr explicit VariantProccessor(Value data) {
+		Node newNode(new NodeType(data));
+		if (this->head.has_value()) newNode->next = head.value();
+		this->head = newNode;
+	}
+	VariantProccessor(const std::initializer_list<Value>& list) {
+		this->head = Merge(list);
+	}
+
+private :
+	constexpr Node Insert(Value data) noexcept {
+		Node newNode(new NodeType(data));
+		if (this->head.has_value()) newNode->next = head.value();
+		return newNode;
+	}
+	constexpr Node BackInsert(Value data) noexcept {
+		Node newNode(new NodeType(data));
+		if (this->head.has_value()) return newNode;
+		Node current = head->value();
+		while (current->next)
+			current = current->next;
+		current->next = newNode;
+		return head->value;
+	}
+
+	constexpr Node Merge(const std::initializer_list<Value>& list) noexcept {
+		Node dummy(new NodeType());
+		Node current = dummy;
+		for (auto& data : list) {
+			Node tmp(new NodeType(data));
+			current->next = tmp;
+			current = current->next;
+		}
+		if(this->head.has_value()) current->next = this->head.value();
+		dummy = dummy->next;
+		return dummy;
+	}
+	constexpr variants Terminate(Node head) noexcept {
+		Node current = head;
+		if (not current->next) {
+			variants gg;
+			return current->data(gg);
+		}
+		std::function<variants(variants)> func = current->data;
+		variants result = Terminate(current->next);
+		return func(result);
+	}
+	constexpr auto Constant(const std::initializer_list<variants>& list) {
+		std::function<variants(variants)> f = [list](variants input) {
+			return list;
+		};
+		return f;
+	}
+	constexpr variants Reduce(const std::initializer_list<std::any>& list) {
+		std::function <variants(variants)> cnst = Constant(list);
+		this->head = BackInsert(cnst);
+		return Terminate(head->value());
+	}
+};
+*/
